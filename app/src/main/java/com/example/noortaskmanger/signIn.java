64a216c897e62c822c890d08ff98e6f67947a990
JamalSamniya <jamal.samniya@gmail.com>
2019-09-23 09:42:13 +0300

    Task

--- app/src/main/java/com/example/noortaskmanger/signIn.java
+++ app/src/main/java/com/example/noortaskmanger/signIn.java
@@ -9,6 +9,8 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageView;
 
+import com.google.firebase.auth.FirebaseAuth;
+
 public class signIn extends AppCompatActivity
 {
  private EditText edtEmail,edtPassword;
@@ -37,5 +39,40 @@ public class signIn extends AppCompatActivity
     {
         String email=edtEmail.getText().toString();
         String passw=edtPassword.getText().toString();
+        boolean isOk=true;
+//        if(email.length()<4)
+//        {
+//            edtEmail.setError("Email length error");
+//            isOk=false;
+//        }
+//        if(email.indexOf("@")<0 || email.indexOf(".")<0)
+//        {
+////            edtEmail.setError("email wrong format");
+//////            isOk=false;
+////        }
+        if ((isValidEmailAddress(email)==false))
+        {
+            edtEmail.setError("Invalid Email");
+            isOk=false;
+        }
+        if(passw.length()<8)
+        {  edtPassword.setError("password length is Error");
+            isOk=false;
+        }
+        if(isOk)
+        {
+
+        }
     }
+    public boolean isValidEmailAddress(String email) {
+        String ePattern = "^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@((\\[[0-9]{1,3}\\.[0-9]{1,3}\\.[0-9]{1,3}\\.[0-9]{1,3}\\])|(([a-zA-Z\\-0-9]+\\.)+[a-zA-Z]{2,}))$";
+        java.util.regex.Pattern p = java.util.regex.Pattern.compile(ePattern);
+        java.util.regex.Matcher m = p.matcher(email);
+        return m.matches();
+    }
+    private void signIn(String email,String passw)
+    {
+        FirebaseAuth auth=FirebaseAuth.getInstance();
+        auth.signInWithEmailAndPassword(email,passw);
+
 }
